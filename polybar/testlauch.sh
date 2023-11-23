59169aaef7583090dea93e6bc8139bda2bd814e0
xiften <6wn5seb9@anonaddy.me>
2023-11-23 13:55:54 -0700

    added polybar

---
+++ polybar/testlauch.sh
@@ -0,0 +1,9 @@
+if type "xrandr" >/dev/null; then
+	while read F1 F2 _; do
+		if [ "$F2" = 'connected' ]; then
+			MONITOR=$F1 polybar --reload main -c ~/.config/polybar/colorblocks/config.ini &
+		fi
+	done <<<$(xrandr)
+else
+	polybar --reload main -c ~/.config/polybar/colorblocks/config.ini &
+fi
